43a59152e3e623db0df7bc3dc0bbb4f92e6e3c58
Niko Steinhoff <niko.steinhoff@pm.me>
2022-12-10 18:42:15 +0100

    Add day 10

---
+++ 10-1.js
@@ -0,0 +1,174 @@
+const example = `addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop`;
+
+require("fs").readFile("10.txt", "utf-8", (err, data) => {
+    const cycles = data
+        .split("\n")
+        .filter(Boolean)
+        .reduce(
+            (cycles, line) => {
+                const {end: begin} = cycles.at(-1);
+                cycles.push({begin, end: begin});
+                if (line === "noop") return cycles;
+
+                const [addx, val] = line.split(" ");
+                cycles.push({begin, end: begin + Number(val)});
+                return cycles;
+            },
+            [{end: 1}],
+        );
+
+    const result = [20, 60, 100, 140, 180, 220].reduce(
+        (sum, cycle) => {
+            const {begin} = cycles[cycle];
+            const signal_strength = begin * cycle;
+            return sum + signal_strength;
+        }, 0
+    );
+
+    console.table(result);
+});
